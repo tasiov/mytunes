f4cd57aff305586eac2238b849ff206debd44fae
tasiov <tasiovictoria@gmail.com>
2016-01-29 16:11:37 -0800

    Create SongQueueView in AppView, link SongQueueView and SongQueueEntryView in index.html, and outline queue views

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,23 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  events: {
+    'click': function() {
+      this.model.play();
+    }
+  },
+
+  intitialize: function() {
+    // this should be a click event of some sort
+
+    this.render();
+  },
+
+  render: function(){
+    return this.$el.html(this.template(this.model.attributes));
+  }
 });
